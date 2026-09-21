e21842a70e95578b1e8b430305e1eea83ad16978
k-sunilreddy <koyyachinnaanu@gmail.com>
2026-09-21 20:06:44 +0530

    Palindrone Number Program with return type and no parameter

---
+++ languagefundamentals/methods/SumofCountDigits.java
@@ -0,0 +1,26 @@
+package com.languagefundamentals.methods;
+
+// calculating sum of count of digits of a number
+public class SumofCountDigits {
+
+	static int a=733070893;
+	static int count=0;
+	static int sum=0;
+	static int sumofCount()
+	{
+		while (a>0)
+		{
+			a=a/10;
+			int c=count++;
+			System.out.println("Digit count:"+c);
+			sum+=c;
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Sum of count digits of a Number:" +sumofCount());
+
+	}
+
+}
